89936a79259a0b9618468cb83c949d8a7c638a40
villain <villain@kali.local>
2021-07-06 15:33:27 +0600

    delete function added

--- second/models.py
+++ second/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class ItemList(models.Model):
+	content = models.CharField(max_length=250)
+	taking_date = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		ordering = ('taking_date',)
+	def __str__(self):
+		return self.content + '(' + str(self.id) + ')'
+
+
--- second/views.py
+++ second/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import ItemList
 
 # Create your views here.
 
 def home(request):
-	return render(request, 'index.html')
+	viewContent=request.POST.get('content')
+	#viewDate=request.POST.get('taking_date')
+	if request.method == 'POST' :
+		dt=ItemList(content=viewContent)
+		dt.save()
+	documents=ItemList.objects.all().order_by('taking_date')
+
+	context={
+		'documents' : documents,
+	}
+	return render(request, 'index.html',context)
+
+def delete_item(request, tid):
+	document=ItemList.objects.get(id=tid)
+	document.delete()
+	print('deleted', tid)
+	#return render(request, 'index.html')
+	return redirect('/')

0b6ac888be12f86958f19c8598df79770f88cafe
villain <villain@kali.local>
2021-07-13 14:49:58 +0600

    Edited and alert are added

--- second/urls.py
+++ second/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home' ),
     path('delete_item/<int:tid>/',views.delete_item ),
+    path('edit/<int:tid>/',views.edit),
 ]
--- second/views.py
+++ second/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import ItemList
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,6 +10,7 @@ def home(request):
 	if request.method == 'POST' :
 		dt=ItemList(content=viewContent)
 		dt.save()
+		return redirect('/') #To solved 'reload and automatically add' problem
 	documents=ItemList.objects.all().order_by('taking_date')
 
 	context={
@@ -22,3 +24,18 @@ def delete_item(request, tid):
 	print('deleted', tid)
 	#return render(request, 'index.html')
 	return redirect('/')
+
+def edit(request, tid):
+	document=ItemList.objects.get(id=tid)
+	context={
+		'document':document,
+	}
+	if request.method == 'POST':
+		contentField=request.POST.get('content') #.get('htmlfield name')
+		dt=ItemList(content=contentField, id=tid)
+		dt.save()
+		messages.success(request,'Successfully Edited')
+		print(contentField)
+		return redirect('/')
+	return render(request, 'edit.html', context)
+	

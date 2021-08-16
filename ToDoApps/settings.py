8669354bdd9a7ad6868e35a38e6e89a7d27aa646
Jewel Mahmud <35461355+MahmudJewel@users.noreply.github.com>
2021-08-17 01:52:37 +0600

    Update settings.py

--- ToDoApps/settings.py
+++ ToDoApps/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z913vx8_t5o#ahodt51t37t4-*9ytn3k*@zi%)lcl26wq=v2k3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*'] #new
+ALLOWED_HOSTS = ['127.0.0.1','dailynotes.herokuapp.com'] #new
 
 
 # Application definition

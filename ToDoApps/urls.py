620296b0f3c4303a9726dc47a5a696834d2ac612
Jewel Mahmud <35461355+MahmudJewel@users.noreply.github.com>
2021-08-17 02:05:35 +0600

    Update urls.py

--- ToDoApps/urls.py
+++ ToDoApps/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),

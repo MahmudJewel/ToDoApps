574fe3bc0670c3b6c32ef35ca37609aeb89ede3c
villain <villain@kali.local>
2021-08-20 01:40:26 +0600

    saved

--- ToDoApps/settings.py
+++ ToDoApps/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-z913vx8_t5o#ahodt51t37t4-*9ytn3k*@zi%)lcl26wq=v2k3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['dailynotes.herokuapp.com', '127.0.0.1'] #new
+ALLOWED_HOSTS = ['*','dailynotes.herokuapp.com','villain07.herokuapp.com', '127.0.0.1'] #new
 
 
 # Application definition
